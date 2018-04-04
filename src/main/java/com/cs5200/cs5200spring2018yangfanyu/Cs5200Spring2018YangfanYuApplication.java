f80f1e46fee289bb010583d4148295cb52058dc6
TianQingChe <tianqingcheyyf@gmail.com>
2018-04-04 16:42:41 -0400

    first commit

---
+++ src/main/java/com/cs5200/cs5200spring2018yangfanyu/Cs5200Spring2018YangfanYuApplication.java
@@ -0,0 +1,12 @@
+package com.cs5200.cs5200spring2018yangfanyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs5200Spring2018YangfanYuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cs5200Spring2018YangfanYuApplication.class, args);
+    }
+}
