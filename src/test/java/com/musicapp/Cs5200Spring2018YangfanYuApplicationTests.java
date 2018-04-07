9a58c848f8c948b903f4c928fe0c3e19271dba63
TianQingChe <tianqingcheyyf@gmail.com>
2018-04-07 01:21:49 -0400

    new

--- src/test/java/com/musicapp/Cs5200Spring2018YangfanYuApplicationTests.java
+++ src/test/java/com/musicapp/Cs5200Spring2018YangfanYuApplicationTests.java
@@ -1,15 +1,10 @@
 package com.musicapp;
 
-<<<<<<< Updated upstream
 import org.junit.Test;
 import org.junit.runner.RunWith;
-=======
 import com.musicapp.web.i18n.I18NService;
 import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
->>>>>>> Stashed changes
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -17,10 +12,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class Cs5200Spring2018YangfanYuApplicationTests {
 
-<<<<<<< Updated upstream
-    @Test
-    public void contextLoads() {
-=======
+
     @Autowired
     private I18NService i18NService;
 
@@ -30,7 +22,6 @@ public class Cs5200Spring2018YangfanYuApplicationTests {
         String messageId = "index.main.callout";
         String actual = i18NService.getMessage(messageId);
         Assert.assertEquals("The actual and expected Strings don't match", expectedResult, actual);
->>>>>>> Stashed changes
     }
 
 }
